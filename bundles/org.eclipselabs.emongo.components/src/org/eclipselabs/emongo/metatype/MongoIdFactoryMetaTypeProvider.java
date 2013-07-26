2ff6421c99b138d2ed75ec20a579fd6c0f739438
Bryan Hunt <bhunt@mac.com>
2013-07-25 22:18:59 -0500

    Added dynamic metatype providers

---
+++ bundles/org.eclipselabs.emongo.components/src/org/eclipselabs/emongo/metatype/MongoIdFactoryMetaTypeProvider.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+
+package org.eclipselabs.emongo.metatype;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.eclipselabs.emongo.config.MongoDatabaseConfigurationProvider;
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.MetaTypeProvider;
+import org.osgi.service.metatype.ObjectClassDefinition;
+
+/**
+ * @author bhunt
+ * 
+ */
+public class MongoIdFactoryMetaTypeProvider implements MetaTypeProvider
+{
+	Set<String> databases = new CopyOnWriteArraySet<String>();
+
+	@Override
+	public String[] getLocales()
+	{
+		return null;
+	}
+
+	@Override
+	public ObjectClassDefinition getObjectClassDefinition(String arg0, String arg1)
+	{
+		AttributeDefinitionImpl database = new AttributeDefinitionImpl("database", "Database", AttributeDefinition.STRING);
+		database.setCardinality(1);
+		database.setDescription("The MongoDB database");
+		database.setOptionLabels(databases.toArray(new String[0]));
+		database.setOptionValues(databases.toArray(new String[0]));
+
+		if (!databases.isEmpty())
+			database.setDefaultValue(new String[] { databases.iterator().next() });
+
+		AttributeDefinitionImpl collection = new AttributeDefinitionImpl("collection", "Collection", AttributeDefinition.STRING);
+		collection.setDescription("The MongoDB collection within the database");
+
+		ObjectClassDefinitionImpl ocd = new ObjectClassDefinitionImpl("org.eclipselabs.emongo.idFactory", "MongoDB ID", "MongoDB ID Provider Configuration");
+		ocd.addAttribute(database);
+		ocd.addAttribute(collection);
+
+		return ocd;
+	}
+
+	public void bindMongoDatabaseConfigurationProvider(MongoDatabaseConfigurationProvider mongoDatabaseConfigurationProvider)
+	{
+		databases.add(mongoDatabaseConfigurationProvider.getAlias());
+	}
+
+	public void unbindMongoDatabaseConfigurationProvider(MongoDatabaseConfigurationProvider mongoDatabaseConfigurationProvider)
+	{
+		databases.remove(mongoDatabaseConfigurationProvider.getAlias());
+	}
+}
---
+++ bundles/org.eclipselabs.emongo.components/src/org/eclipselabs/emongo/metatype/ObjectClassDefinitionImpl.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+
+package org.eclipselabs.emongo.metatype;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.ObjectClassDefinition;
+
+/**
+ * @author bhunt
+ * 
+ */
+public class ObjectClassDefinitionImpl implements ObjectClassDefinition
+{
+	private Collection<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+	private String description;
+	private String id;
+	private String name;
+
+	public ObjectClassDefinitionImpl(String id, String name, String description)
+	{
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public void addAttribute(AttributeDefinition attributeDefinition)
+	{
+		attributeDefinitions.add(attributeDefinition);
+	}
+
+	@Override
+	public AttributeDefinition[] getAttributeDefinitions(int arg0)
+	{
+		return attributeDefinitions.toArray(new AttributeDefinition[0]);
+	}
+
+	@Override
+	public String getDescription()
+	{
+		return description;
+	}
+
+	@Override
+	public String getID()
+	{
+		return id;
+	}
+
+	@Override
+	public InputStream getIcon(int arg0) throws IOException
+	{
+		return null;
+	}
+
+	@Override
+	public String getName()
+	{
+		return name;
+	}
+}
